650093dc988c76917b4e3efe9ac9b36089d2572e
Patrick Fink <finkp@us.ibm.com>
2018-11-25 10:11:18 -0500

    Initial Design

--- controller/ViewController.swift
+++ controller/ViewController.swift
@@ -15,6 +15,26 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var myLog: UITextView!
+    @IBOutlet weak var myStatus: UILabel!
+    @IBOutlet weak var myInput: UITextField!
+   
+    @IBAction func btnAdd(_ sender: Any) {
+       add()
+    }
+    @IBAction func btnClear(_ sender: Any) {
+        myInput.text = ""
+        myLog.text = "Status Log"
+    }
+    
+    func add() {
+        myStatus.text = "Adding message..."
+        myLog.text = String(myLog.text!) + "\n" + String(myInput.text!)
+    }
+    
+    @IBAction func enter(_ sender: UITextField) {
+        add()
+    }
+    
 }
 
